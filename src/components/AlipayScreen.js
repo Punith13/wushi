4edd449e26d92a08ed0f20e58a9767ddf8330325
Punith13 <puneethu@gmail.com>
2018-04-22 08:33:34 +1000

    Drawer open

--- src/components/AlipayScreen.js
+++ src/components/AlipayScreen.js
@@ -7,96 +7,104 @@ import Modal from 'material-ui/Modal';
 import List, { ListItem } from 'material-ui/List';
 import { openCloseModal } from '../Actions';
 
-
 function rand() {
-  return Math.round(Math.random() * 20) - 10;
+	return Math.round(Math.random() * 20) - 10;
 }
 
 function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
+	const top = 50 + rand();
+	const left = 50 + rand();
 
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  };
+	return {
+		top: `${top}%`,
+		left: `${left}%`,
+		transform: `translate(-${top}%, -${left}%)`
+	};
 }
 
 const styles = theme => ({
-  paper: {
-    position: 'absolute',
-    width: theme.spacing.unit * 50,
-    backgroundColor: theme.palette.background.paper,
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing.unit * 4,
-  },
+	paper: {
+		position: 'absolute',
+		width: theme.spacing.unit * 50,
+		backgroundColor: theme.palette.background.paper,
+		boxShadow: theme.shadows[5],
+		padding: theme.spacing.unit * 4
+	}
 });
 
 class SimpleModal extends React.Component {
-  state = {
-    open: true,
-  };
+	state = {
+		open: true
+	};
 
-  handleOpen = () => {
-    this.setState({ open: true });
-  };
+	handleOpen = () => {
+		this.setState({ open: true });
+	};
 
-  handleClose = () => {
-    this.props.openCloseModal(false);
-  };
+	handleClose = () => {
+		this.props.openCloseModal(false);
+	};
 
-  render() {
-    const { classes } = this.props;
-    return (
-      <div>
-        <Modal
-          aria-labelledby="simple-modal-title"
-          aria-describedby="simple-modal-description"
-          open={this.props.open}
-          onClose={this.handleClose}
-        >
-          <div style={getModalStyle()} className={classes.paper}>
-            <List
-                component="nav"
-            >
-            <ListItem>
-                <Typography variant="headline" id="modal-title">
-                Powered by Wushi - Alipay
-                </Typography>
-            </ListItem>
-            <ListItem>
-                <Typography variant="title" id="modal-title">
-                    Tong's Garden
-                </Typography>
-            </ListItem>
-            <ListItem>
-                <Typography variant="display1" gutterBottom>
-                    Pay : AUD $10
-                </Typography>
-            </ListItem>
-            </List>
-            { this.props.alipayImage && <img src={this.props.alipayImage} alt="scan code" style={{ width: '200px', height: '200px' }} /> }
-            { !this.props.alipayImage &&  <Typography variant="subheading" gutterBottom> Generating Scan code... </Typography> }
-            <SimpleModalWrapped />
-          </div>
-        </Modal>
-      </div>
-    );
-  }
+	render() {
+		const { classes } = this.props;
+		return (
+			<div>
+				<Modal
+					aria-labelledby="simple-modal-title"
+					aria-describedby="simple-modal-description"
+					open={this.props.open}
+					onClose={this.handleClose}
+				>
+					<div style={getModalStyle()} className={classes.paper}>
+						<List component="nav">
+							<ListItem>
+								<Typography variant="headline" id="modal-title">
+									Powered by Wushi - Alipay
+								</Typography>
+							</ListItem>
+							<ListItem>
+								<Typography variant="title" id="modal-title">
+									Tong's Garden
+								</Typography>
+							</ListItem>
+							<ListItem>
+								<Typography variant="display1" gutterBottom>
+									{`Pay : AUD ${this.props.amount}`}
+								</Typography>
+							</ListItem>
+						</List>
+						{this.props.alipayImage && (
+							<img
+								src={this.props.alipayImage}
+								alt="scan code"
+								style={{ width: '200px', height: '200px' }}
+							/>
+						)}
+						{!this.props.alipayImage && (
+							<Typography variant="subheading" gutterBottom>
+								{' '}
+								Generating Scan code...{' '}
+							</Typography>
+						)}
+						<SimpleModalWrapped />
+					</div>
+				</Modal>
+			</div>
+		);
+	}
 }
 
 SimpleModal.propTypes = {
-  classes: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired
 };
 
 const mapStateToProps = ({ alipayState }) => {
-    return { 
-        alipayImage: alipayState.alipayImage
-    }
-}
+	return {
+		alipayImage: alipayState.alipayImage
+	};
+};
 
 // We need an intermediary variable for handling the recursive nesting.
-const SimpleModalWrapped = withStyles(styles)(connect(mapStateToProps, {openCloseModal})(SimpleModal));
+const SimpleModalWrapped = withStyles(styles)(connect(mapStateToProps, { openCloseModal })(SimpleModal));
 
 export default SimpleModalWrapped;
